d3e0b08666b6acab901061baf837c7ab82451b48
Kartoshechka <Kartohan@gmail.com>
2022-11-30 15:29:31 +0200

    Add Login and protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,31 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Category from "./components/Category";
 import Post from "./components/Post";
+import { useState } from "react";
+import PrivateRoutes from "./components/PrivateRoutes";
+import Login from "./components/Login";
+import { useJwt } from "react-jwt";
 
 function App() {
+  const [token, setToken] = useState(localStorage.getItem("token"));
+  const { decodedToken, isExpired } = useJwt(token);
   return (
     <div className="App">
       <Header />
       <div className="container mx-auto px-4 mb-8">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/category/:category_id" element={<Category />} />
-          <Route path="/posts/:post_id" element={<Post />} />
+          <Route element={<PrivateRoutes isExpired={isExpired} />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/category/:category_id" element={<Category />} />
+            <Route path="/posts/:post_id" element={<Post />} />
+          </Route>
+          <Route
+            path="/login"
+            element={<Login setToken={setToken} isExpired={isExpired} />}
+          />
         </Routes>
       </div>
-      <div className="bg-rose-50 px-12 py-3 block rounded-lg mx-auto w-fit">
+      <div className="bg-rose-50 px-12 py-3 block rounded-lg mx-auto w-fit my-3">
         Created by{" "}
         <a className="text-rose-600" href="https://github.com/Kartohan">
           {" "}
---
+++ src/components/Login.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const Login = ({ setToken, isExpired }) => {
+  const [form, setForm] = useState({
+    username: "",
+    password: "",
+  });
+  const [data, setData] = useState(null);
+  const navigate = useNavigate();
+  const handleChangeForm = (e) => {
+    const { name, value } = e.target;
+    setForm((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post(`http://localhost:3001/api/users/login`, form).then((res) => {
+      localStorage.setItem("token", res.data.token);
+      setToken(res.data.token);
+      if (localStorage.getItem("token")) {
+        navigate("/");
+      }
+    });
+  };
+
+  useEffect(() => {
+    if (!isExpired) {
+      navigate("/");
+    }
+  }, []);
+
+  return (
+    <div>
+      <div className="text-center font-bold text-2xl my-5">Log In</div>
+      <form
+        className="min-w-fit w-[33%] mx-auto"
+        action=""
+        onSubmit={handleSubmit}
+      >
+        <div className="w-full flex flex-col">
+          <label htmlFor="username">Username</label>
+          <input
+            className="p-2 rounded-md bg-slate-100 focus:outline-none focus:ring-blue-500 focus:ring-1 transition hover:shadow-[0px_0px_3px_rgba(0,8,255,0.4)]"
+            type="text"
+            name="username"
+            id="username"
+            onChange={handleChangeForm}
+            required
+          />
+        </div>
+        <div className="w-full flex flex-col">
+          <label htmlFor="">Password</label>
+          <input
+            className="p-2 rounded-md bg-slate-100 focus:outline-none focus:ring-blue-500 focus:ring-1 transition hover:shadow-[0px_0px_3px_rgba(0,8,255,0.4)]"
+            type="password"
+            name="password"
+            id="password"
+            onChange={handleChangeForm}
+            required
+          />
+        </div>
+        <input
+          className="py-2 px-10 bg-rose-100 text-lg rounded-lg mt-8 hover:bg-rose-400 transition scale-[1.02] block mx-auto"
+          type="submit"
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Login;
